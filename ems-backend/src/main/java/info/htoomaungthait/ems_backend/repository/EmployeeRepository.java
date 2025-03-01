b19ea5c7fff738f4f34b44cd877693ec05a9d5f4
Htoo Maung Thait <htoomaungthait@gmail.com>
2025-03-01 23:04:44 +0630

    employee repository Jpa repository extended

---
+++ ems-backend/src/main/java/info/htoomaungthait/ems_backend/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package info.htoomaungthait.ems_backend.repository;
+
+import info.htoomaungthait.ems_backend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
