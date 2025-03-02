d09c73c4955f0ca151b718c11f967e783cc334a0
Htoo Maung Thait <htoomaungthait@gmail.com>
2025-03-02 07:39:32 +0630

    used written code instead of lombok

--- ems-backend/src/main/java/info/htoomaungthait/ems_backend/dto/EmployeeDto.java
+++ ems-backend/src/main/java/info/htoomaungthait/ems_backend/dto/EmployeeDto.java
@@ -1,14 +1,9 @@
 package info.htoomaungthait.ems_backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-//@AllArgsConstructor
+/**
+ * To use data transfer object of employee
+ */
 public class EmployeeDto {
 
     public EmployeeDto(Long id, String firstName, String lastName, String email) {
@@ -18,8 +13,44 @@ public class EmployeeDto {
         this.email = email;
     }
 
+    public EmployeeDto() {
+
+    }
+
     private Long id;
     private String firstName;
     private String lastName;
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
