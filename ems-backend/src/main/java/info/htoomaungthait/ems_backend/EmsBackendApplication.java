240034a8eb10155b584a9f9b753a76d833e9fa01
Htoo Maung Thait <htoomaungthait@gmail.com>
2025-03-02 20:10:03 +0630

    dot env work with env.properties

--- ems-backend/src/main/java/info/htoomaungthait/ems_backend/EmsBackendApplication.java
+++ ems-backend/src/main/java/info/htoomaungthait/ems_backend/EmsBackendApplication.java
@@ -1,5 +1,6 @@
 package info.htoomaungthait.ems_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,15 +8,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
-//		Dotenv dotenv = Dotenv.load();
-//
-//		String dbHost = dotenv.get("DB_HOST");
-//		String dbPort = dotenv.get("DB_PORT");
-//
-//		System.out.println("Database Host: " + dbHost);
-//		System.out.println("Database Port: " + dbPort);
+		Dotenv dotenv = Dotenv.configure().load();
+
+		String dbHost = dotenv.get("DB_HOST");
+		String dbPort = dotenv.get("DB_PORT");
+		String dbName = dotenv.get("DB_NAME");
+		String dbUsername = dotenv.get("DB_USERNAME");
+		String dbPassword = dotenv.get("DB_PASSWORD");
+
+		System.out.println("Database Host: " + dbHost);
+		System.out.println("Database Port: " + dbPort);
+		System.out.println("Database Name: " + dbName);
+		System.out.println("Database Username: " + dbUsername);
+		System.out.println("Database Password: " + dbPassword);
 
 		SpringApplication.run(EmsBackendApplication.class, args);
+
+		System.out.print("This is main app!");
 
 
 	}
