81f8637a1e0e66f965b080818e47ba44258696a3
Htoo Maung Thait <htoomaungthait@gmail.com>
2025-06-22 23:56:58 +0630

    project initialization done

---
+++ flywayspringpractice/src/main/java/info/htoomgthait/flywayspringpractice/FlywayspringpracticeApplication.java
@@ -0,0 +1,15 @@
+package info.htoomgthait.flywayspringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayspringpracticeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FlywayspringpracticeApplication.class, args);
+		System.out.println("Flyway Spring Practice Application is running!");
+	}
+
+}
