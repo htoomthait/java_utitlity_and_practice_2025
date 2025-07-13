99565d46f3b421f2369e7b3f30ea076d90a89aa9
Htoo Maung Thait <htoomaungthait@gmail.com>
2025-07-13 22:48:19 +0630

    liquid database new app created and started to run

---
+++ Liquid-Database-Practice/src/main/java/com/example/liquid/database/practice/LiquidDatabasePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquid.database.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidDatabasePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidDatabasePracticeApplication.class, args);
+	}
+
+}
---
+++ Liquid-Database-Practice/src/main/java/com/example/liquid/database/practice/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.liquid.database.practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Welcome to the Liquid Database Practice Application!";
+    }
+}
